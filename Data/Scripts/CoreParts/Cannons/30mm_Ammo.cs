516fd10f9be313eadd49afc659658fa3cd749ebe
Andrew <sgtdrew720@users.noreply.github.com>
2024-10-18 10:44:42 -0500

    Update 30mm_Ammo.cs
    increased 30mm AOE damage

--- Data/Scripts/CoreParts/Cannons/30mm_Ammo.cs
+++ Data/Scripts/CoreParts/Cannons/30mm_Ammo.cs
@@ -135,9 +135,9 @@ namespace Scripts
                 {
                     Enable = true,
                     Radius = 1.5f, // Radius of AOE effect, in meters.
-                    Damage = 150f,
+                    Damage = 200f,
                     Depth = 1f, // Max depth of AOE effect, in meters. 0=disabled, and AOE effect will reach to a depth of the radius value
-                    MaxAbsorb = 300f, // Soft cutoff for damage, except for pooled falloff.  If pooled falloff, limits max damage per block.
+                    MaxAbsorb = 400f, // Soft cutoff for damage, except for pooled falloff.  If pooled falloff, limits max damage per block.
                     Falloff = Exponential, //.NoFalloff applies the same damage to all blocks in radius
                     //.Linear drops evenly by distance from center out to max radius
                     //.Curve drops off damage sharply as it approaches the max radius
